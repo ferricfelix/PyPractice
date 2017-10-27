bc0495ec95b9e0a3d3faed7e45f7c80d9ce7e1bc
Gene Shumkov <shumkov@uchicago.edu>
2017-10-26 21:31:01 -0500

    week 5

--- Python MPCS 51042-2/week5/week5notes.py
+++ Python MPCS 51042-2/week5/week5notes.py
@@ -16,6 +16,39 @@ finally:
 	f.close()
 
 #example of using with to not require the finally clause
+# variable f will still exist outside the scope, but is closed
+# so if you want to reopen the file, you will need to open it again
+# for more info, look at the standard library at docs.python.org
 with open('data.csv', 'r') as f:
 	#do some work with the file
+
+# Context managers require that a class have two defined methods:
+__enter__()
+__exit__()
+
+with obj as:
+	...
+# is roughly equivalent to
+try:
+	x = obj.__enter__()
+	...
+finally
+	obj.__exit__()
+
+# usually takes no arguments and returs self
+__enter__() 
+
+# decimal allows to you to go more precise than float
+from math import pi
+print(pi)
+# vs
+from decimal import Decimal, localcontext
+
+with localcontext() as ctx:
+	ctx.prec = 100
+	d = Decimal('1')/Decimal('7')
+	print(d)
+	# get 100 points of precision
+
+
 
